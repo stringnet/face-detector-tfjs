51d04bee0633c81bcc3f02a996cd165f5a1bf042
stringnet <135693628+stringnet@users.noreply.github.com>
2025-05-03 14:43:57 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,14 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
+  }
+});
