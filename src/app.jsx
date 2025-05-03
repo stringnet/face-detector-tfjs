d0337f9dc728e4e1ea37aa9d535a6b262c55ef77
stringnet <135693628+stringnet@users.noreply.github.com>
2025-05-03 14:56:17 -0500

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import FaceDetector from './components/FaceDetector';
 
+function App() {
+  return (
+    <div>
+      <h2 style={{ textAlign: 'center' }}>FaceDetector (TensorFlow.js)</h2>
+      <FaceDetector />
+    </div>
+  );
+}
+
+export default App;
