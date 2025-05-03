38f55912a4c369f61741ef4b953e11f4500b00e3
stringnet <135693628+stringnet@users.noreply.github.com>
2025-05-03 14:55:34 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,18 +8,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
-
-// face-detector-client/src/App.jsx
-import React from 'react';
-import FaceDetector from './components/FaceDetector';
-
-function App() {
-  return (
-    <div>
-      <h2 style={{ textAlign: 'center' }}>FaceDetector (TensorFlow.js)</h2>
-      <FaceDetector />
-    </div>
-  );
-}
-
-export default App;
